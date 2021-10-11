04010b5a1d382b4e669c31ba52625a22f16b83fa
dimaTsaruk <dima.beseder@gmail.com>
2021-10-11 17:21:01 -0500

    microsoft executive office

---
+++ src/homework/test.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class test {
+    public static void main(String[] args) {
+        String e = "@microsoft.com";
+        String
+                CCO = "Judson Althoff: Judson.Althoff",
+                CMO = "Chris Caposella: Chris.Caposella",
+                CHR = "Kathleen Hogan: Kathleen.Hogan",
+                CFO = "Amy Hkood: Amy.Hkood",
+                CEO = "Satya Nadella: Satya.Nadella",
+                VC = "Brad Smith: Brad.Smith",
+                EVP = "Christoper Young: Christoper.Young";
+
+        System.out.printf("CCO - %s%s\nCMO - %s%s\nCHR - %s%s\nCFO - %s%s\nCEO - %s%s\nVC - %s%s\nEVP - %s%s\n",
+                CCO, e, CMO, e, CHR, e, CFO, e, CEO, e, VC, e, EVP, e);
+
+
+    }
+}
